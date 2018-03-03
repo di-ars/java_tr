84fb33c18737228887a86b2f416f398a74100645
di <36971008+di-ars@users.noreply.github.com>
2018-03-03 22:40:09 +0300

    Created Square and Rectangle classes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,13 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        Square s = new Square(5);
+        System.out.println("Area of square with side length equal to " + s.l + " is: " + s.area());
+
+        Rectangle r = new Rectangle(5, 6);
+        System.out.println("Area of rectangle with side lengths equal to " + r.a + " and " + r.b + " is: " + r.area());
     }
+
 
 }
