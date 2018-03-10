6e91aa645b6bdfd1f4e10d54be47dbfa4b4233b1
di <36971008+di-ars@users.noreply.github.com>
2018-03-10 22:00:01 +0300

    ApplicationManager class created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,15 +1,14 @@
 package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.Test;
-import org.openqa.selenium.*;
 
 public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
-        gotoGroupPage();
-        selectGroup();
-        deleteSelectedGroups();
-        returnToGroupPage();
+        app.gotoGroupPage();
+        app.selectGroup();
+        app.deleteSelectedGroups();
+        app.returnToGroupPage();
     }
 
 }
