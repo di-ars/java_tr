b221032f75cd9f62978329c98c84fb9970bf1ff4
di <36971008+di-ars@users.noreply.github.com>
2018-03-11 22:30:47 +0300

    excess TestBase class deleted

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.TestBase;
 
 public class GroupCreationTests extends TestBase {
 
