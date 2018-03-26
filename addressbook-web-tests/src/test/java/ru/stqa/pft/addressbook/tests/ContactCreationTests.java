13df7628f8376595e87ee518fa2de3d9c4cdbf46
di <36971008+di-ars@users.noreply.github.com>
2018-03-26 23:13:54 +0300

    verification of contacts count and comparing of contacts lists after contact creation are added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,15 +1,28 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
         ContactData contactData = new ContactData("test", "", "test", "", null, "", "", "test", "12345", "", "", "", "contact@contact.com", "", "", "", null, null, "test11", "", "", "");
+        List<ContactData> contactsBefore = app.getContactHelper().getContactsList();
         app.getNavigationHelper().gotoContactCreationPage();
         app.getContactHelper().createContact(contactData);
         app.getNavigationHelper().gotoHomePage();
+        List<ContactData> contactsAfter = app.getContactHelper().getContactsList();
+        Assert.assertEquals(contactsAfter.size(), contactsBefore.size() + 1, "Contacts count must be enlarged at 1 after new contact created");
+
+        contactsBefore.add(contactData);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        contactsBefore.sort(byId);
+        contactsAfter.sort(byId);
+        Assert.assertEquals(contactsAfter, contactsBefore);
     }
 }
