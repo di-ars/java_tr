8032c1bd5fbd8761ac4e2c4e9f139097861e76e0
di <36971008+di-ars@users.noreply.github.com>
2018-04-16 14:33:07 +0300

    local.properties with description of environment is added (baseUrl,adminLogin,adminPassword)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,7 +7,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
     //protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
     //protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
