5a2821becc70df49b9f7f6a03b858b4317ac1973
di <36971008+di-ars@users.noreply.github.com>
2018-04-21 15:29:24 +0300

    ContactsInGroupsTests: contact is added to existing group (if possible)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactsInGroupsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactsInGroupsTests.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
@@ -13,6 +14,13 @@ public class ContactsInGroupsTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
+        Groups groups = app.db().groups();
+        if (groups.size() == 0) {
+            GroupData group = new GroupData().withName("test_group");
+            app.goTo().groupPage();
+            app.group().create(group);
+        }
+
         if (app.db().contacts().size() == 0) {
             app.goTo().contactCreationPage();
             app.contact().createContact(new ContactData()
@@ -23,7 +31,7 @@ public class ContactsInGroupsTests extends TestBase {
 
     @Test
     public void testContactAddedToGroup() {
-        GroupData groupToAdd = new GroupData().withName("test_group").withHeader("header").withFooter("footer");
+        /*GroupData groupToAdd = new GroupData().withName("test_group").withHeader("header").withFooter("footer");
         app.goTo().groupPage();
         app.group().create(groupToAdd);
         groupToAdd.withId(app.db().groups().stream().mapToInt((g) -> g.getId()).max().getAsInt());//set proper (max) id to created group
@@ -34,18 +42,47 @@ public class ContactsInGroupsTests extends TestBase {
         app.contact().addToGroup(contact, groupToAdd);
         contact = app.db().contactById(contact.getId());
         Groups groupsAfter = contact.getGroups();
+        assertThat(groupsAfter, equalTo(groupsBefore.withAdded(groupToAdd)));*/
+
+        ContactData contactToUse = null;
+        GroupData groupToAdd = null;
+        Groups groupsBefore = null;
+        Groups groups = app.db().groups();
+        //look for contact which can be added to existing group
+        Contacts contacts = app.db().contacts();
+        for (ContactData contact : contacts) {
+            groupsBefore = contact.getGroups();
+            if (groupsBefore.size() != groups.size()) { //checks if contact isn't in all existing groups
+                for (GroupData group : groups) {
+                    if (!groupsBefore.contains(group)) {
+                        contactToUse = contact;
+                        groupToAdd = group;
+                        break;
+                    }
+                }
+            }
+            if (contactToUse != null) { //if contact found
+                break;
+            }
+        }
+        if (contactToUse == null) { //all contacts are in all existing group => create new group and use any contact
+            groupToAdd = new GroupData().withName("test_group").withHeader("header").withFooter("footer");
+            app.goTo().groupPage();
+            app.group().create(groupToAdd);
+            groupToAdd.withId(app.db().groups().stream().mapToInt((g) -> g.getId()).max().getAsInt());//set proper (max) id to created group
+            contactToUse = app.db().contacts().iterator().next();
+        }
+
+        app.contact().openHomePage();
+        app.contact().addToGroup(contactToUse, groupToAdd);
+        contactToUse = app.db().contactById(contactToUse.getId());
+        Groups groupsAfter = contactToUse.getGroups();
         assertThat(groupsAfter, equalTo(groupsBefore.withAdded(groupToAdd)));
+
     }
 
     @Test
     public void testContactDeletedFromGroup() {
-        Groups groups = app.db().groups();
-        if (groups.size() == 0) {
-            GroupData group = new GroupData().withName("test_group");
-            app.goTo().groupPage();
-            app.group().create(group);
-        }
-
         GroupData groupToDeleteFrom;
         app.contact().openHomePage();
         ContactData contact = app.db().contacts().iterator().next();
